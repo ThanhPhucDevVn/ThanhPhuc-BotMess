09e4fdb02405bc9d18e54571687c537f0d1f1c5f
ThanhPhuc <contact.thanhphucdev@gmail.com>
2025-08-02 17:42:06 +0700

    Update rs.js

--- modules/commands/rs.js
+++ modules/commands/rs.js
@@ -2,10 +2,11 @@ this.config = {
     name: "rs",
     version: "1.0.0",
     hasPermssion: 3,
-    credits: "DongDev",
+    credits: "ThanhPhuc",
     description: "Khởi Động Lại Bot.",
     commandCategory: "Admin",
     cooldowns: 0,
     images: [],
  };
+
  this.run = ({event, api}) => api.sendMessage("✅", event.threadID, () => process.exit(1), event.messageID)
